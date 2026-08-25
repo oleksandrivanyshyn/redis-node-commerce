323992dacf76e59d86bef2d55fac77b971c06833
Oleksandr Ivanyshyn <o.ivanyshyn.dev@gmail.com>
2026-08-26 01:10:40 +0300

    feat: implement Redis caching for specific page routes

--- src/services/queries/page-cache.ts
+++ src/services/queries/page-cache.ts
@@ -1,3 +1,12 @@
-export const getCachedPage = (route: string) => {};
+import { client } from '$services/redis';
+const cacheRoutes = ['/about', '/privacy', '/auth/signin', '/auth/signup'];
 
-export const setCachedPage = (route: string, page: string) => {};
+export const getCachedPage = (route: string) => {
+  if (cacheRoutes.includes(route)) return client.get('pagecache#' + route);
+  return null;
+};
+
+export const setCachedPage = (route: string, page: string) => {
+  if (cacheRoutes.includes(route))
+    client.set('pagecache#' + route, page, { EX: 2 });
+};
