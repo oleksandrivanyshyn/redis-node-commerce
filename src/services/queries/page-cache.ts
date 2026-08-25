5ab6842d7cd96e889c915a7302f25ba4b454c215
Oleksandr Ivanyshyn <o.ivanyshyn.dev@gmail.com>
2026-08-26 01:12:12 +0300

    refactor: extract page cache key logic into a reusable function

--- src/services/queries/page-cache.ts
+++ src/services/queries/page-cache.ts
@@ -1,12 +1,13 @@
 import { client } from '$services/redis';
+import { pageCacheKey } from '$services/keys';
 const cacheRoutes = ['/about', '/privacy', '/auth/signin', '/auth/signup'];
 
 export const getCachedPage = (route: string) => {
-  if (cacheRoutes.includes(route)) return client.get('pagecache#' + route);
+  if (cacheRoutes.includes(route)) return client.get(pageCacheKey(route));
   return null;
 };
 
 export const setCachedPage = (route: string, page: string) => {
   if (cacheRoutes.includes(route))
-    client.set('pagecache#' + route, page, { EX: 2 });
+    client.set(pageCacheKey(route), page, { EX: 2 });
 };
