8311647c8452e95b0d354a76e6b8abc0060386d5
Oleksandr Ivanyshyn <o.ivanyshyn.dev@gmail.com>
2026-08-27 01:20:53 +0300

    feat: implement itemsByViews query with Redis sorted set and sorting options

--- src/services/queries/items/by-views.ts
+++ src/services/queries/items/by-views.ts
@@ -1,1 +1,20 @@
-export const itemsByViews = async (order: 'DESC' | 'ASC' = 'DESC', offset = 0, count = 10) => {};
+import { client } from '$services/redis';
+import { itemsKey, itemsByViewsKey } from '$services/keys';
+
+export const itemsByViews = async (
+  order: 'DESC' | 'ASC' = 'DESC',
+  offset = 0,
+  count = 10
+) => {
+  const results = await client.sort(itemsByViewsKey(), {
+    GET: ['#', `${itemsKey('*')}->name`, `${itemsKey('*')}->views`],
+    BY: 'nosort',
+    DIRECTION: order,
+    LIMIT: {
+      offset,
+      count,
+    },
+  });
+
+  console.log(results);
+};
