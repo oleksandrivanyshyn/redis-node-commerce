7588f002bfea01ca4a4d12d03621580a1a4cce4f
Oleksandr Ivanyshyn <o.ivanyshyn.dev@gmail.com>
2026-08-27 01:29:33 +0300

    feat: extend itemsByViews query to include additional item attributes and deserialization

--- src/services/queries/items/by-views.ts
+++ src/services/queries/items/by-views.ts
@@ -1,13 +1,21 @@
 import { client } from '$services/redis';
 import { itemsKey, itemsByViewsKey } from '$services/keys';
+import { deserialize } from './deserialize';
 
 export const itemsByViews = async (
   order: 'DESC' | 'ASC' = 'DESC',
   offset = 0,
   count = 10
 ) => {
-  const results = await client.sort(itemsByViewsKey(), {
-    GET: ['#', `${itemsKey('*')}->name`, `${itemsKey('*')}->views`],
+  let results: any = await client.sort(itemsByViewsKey(), {
+    GET: [
+      '#',
+      `${itemsKey('*')}->name`,
+      `${itemsKey('*')}->views`,
+      `${itemsKey('*')}->endingAt`,
+      `${itemsKey('*')}->imageUrl`,
+      `${itemsKey('*')}->price`,
+    ],
     BY: 'nosort',
     DIRECTION: order,
     LIMIT: {
@@ -16,5 +24,13 @@ export const itemsByViews = async (
     },
   });
 
-  console.log(results);
+  const items = [];
+  while (results.length) {
+    const [id, name, views, endingAt, imageUrl, price, ...rest] = results;
+    const item = deserialize(id, { name, views, endingAt, imageUrl, price });
+    items.push(item);
+    results = rest;
+  }
+
+  return items;
 };
