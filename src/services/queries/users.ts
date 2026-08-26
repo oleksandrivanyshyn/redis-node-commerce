e13b1d5ec5a999e633cf95fd61f575ea34dd4e6b
Oleksandr Ivanyshyn <o.ivanyshyn.dev@gmail.com>
2026-08-26 16:42:34 +0300

    feat: implement getUserByUsername with Redis user retrieval and error handling

--- src/services/queries/users.ts
+++ src/services/queries/users.ts
@@ -3,7 +3,15 @@ import { genId } from '$services/utils';
 import { client } from '$services/redis';
 import { usernamesKey, usernamesUniqueKey, usersKey } from '$services/keys';
 
-export const getUserByUsername = async (username: string) => {};
+export const getUserByUsername = async (username: string) => {
+  const decimalId = await client.zScore(usernamesKey(), username);
+  if (!decimalId) {
+    throw new Error(`User with username ${username} does not exist`);
+  }
+  const id = decimalId.toString(16);
+  const user = await client.hGetAll(usersKey(id));
+  return deserialize(id, user);
+};
 
 export const getUserById = async (id: string) => {
   const user = await client.hGetAll(usersKey(id));
