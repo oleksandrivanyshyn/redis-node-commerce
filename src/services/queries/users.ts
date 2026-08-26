6862d3e5b40b49732d537c40316727eadc21b1e7
Oleksandr Ivanyshyn <o.ivanyshyn.dev@gmail.com>
2026-08-26 15:29:08 +0300

    feat: enforce unique usernames in createUser with Redis set

--- src/services/queries/users.ts
+++ src/services/queries/users.ts
@@ -1,7 +1,7 @@
 import type { CreateUserAttrs } from '$services/types';
 import { genId } from '$services/utils';
 import { client } from '$services/redis';
-import { usersKey } from '$services/keys';
+import { usernamesUniqueKey, usersKey } from '$services/keys';
 
 export const getUserByUsername = async (username: string) => {};
 
@@ -12,7 +12,11 @@ export const getUserById = async (id: string) => {
 
 export const createUser = async (attrs: CreateUserAttrs) => {
   const id = genId();
+  const exists = await client.sIsMember(usernamesUniqueKey(), attrs.username);
+  if (exists) throw new Error(`Username ${attrs.username} already exists`);
+
   await client.hSet(usersKey(id), serialize(attrs));
+  await client.sAdd(usernamesUniqueKey(), attrs.username);
   return id;
 };
 
