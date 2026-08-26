72b1cb6d7550403bbf74a34aaa1ae735fafd10f2
Oleksandr Ivanyshyn <o.ivanyshyn.dev@gmail.com>
2026-08-27 01:04:37 +0300

    feat: implement itemsByEndingTime query with Redis sorted set and deserialization

--- src/services/queries/items/by-ending-time.ts
+++ src/services/queries/items/by-ending-time.ts
@@ -1,5 +1,24 @@
+import { client } from '$services/redis';
+import { itemsKey, itemsByEndingAtKey } from '$services/keys';
+import { deserialize } from './deserialize';
+
 export const itemsByEndingTime = async (
-	order: 'DESC' | 'ASC' = 'DESC',
-	offset = 0,
-	count = 10
-) => {};
+  order: 'DESC' | 'ASC' = 'DESC',
+  offset = 0,
+  count = 10
+) => {
+  const ids = await client.zRange(itemsByEndingAtKey(), Date.now(), '+inf', {
+    BY: 'SCORE',
+    LIMIT: {
+      offset,
+      count,
+    },
+  });
+
+  const results = await Promise.all(
+    ids.map((id) => client.hGetAll(itemsKey(id)))
+  );
+
+  console.log(results);
+  return results.map((item, i) => deserialize(ids[i], item));
+};
