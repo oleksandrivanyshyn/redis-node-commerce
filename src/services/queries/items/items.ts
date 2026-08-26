43f0ae94d53437fc95f3122655c363964aa0030e
Oleksandr Ivanyshyn <o.ivanyshyn.dev@gmail.com>
2026-08-26 12:52:03 +0300

    feat: implement createItem with Redis hash storage and add itemsKey helper function

--- src/services/queries/items/items.ts
+++ src/services/queries/items/items.ts
@@ -1,7 +1,19 @@
 import type { CreateItemAttrs } from '$services/types';
+import { client } from '$services/redis';
+import { serialize } from './serialize';
+import { genId } from '$services/utils';
+import { itemsKey } from '$services/keys';
 
 export const getItem = async (id: string) => {};
 
 export const getItems = async (ids: string[]) => {};
 
-export const createItem = async (attrs: CreateItemAttrs, userId: string) => {};
+export const createItem = async (attrs: CreateItemAttrs) => {
+  const id = genId();
+
+  const serialized = serialize(attrs);
+
+  await client.hSet(itemsKey(id), serialized);
+
+  return id;
+};
