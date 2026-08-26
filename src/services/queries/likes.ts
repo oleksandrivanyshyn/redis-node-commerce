5b3e389a149c34604eac724875a62a15cf1f68f7
Oleksandr Ivanyshyn <o.ivanyshyn.dev@gmail.com>
2026-08-26 15:31:30 +0300

    feat: implement userLikesKey helper and likeItem functionality with Redis set

--- src/services/queries/likes.ts
+++ src/services/queries/likes.ts
@@ -1,9 +1,17 @@
+import { client } from '$services/redis';
+import { userLikesKey } from '$services/keys';
+
 export const userLikesItem = async (itemId: string, userId: string) => {};
 
 export const likedItems = async (userId: string) => {};
 
-export const likeItem = async (itemId: string, userId: string) => {};
+export const likeItem = async (itemId: string, userId: string) => {
+  await client.sAdd(userLikesKey(userId), itemId);
+};
 
 export const unlikeItem = async (itemId: string, userId: string) => {};
 
-export const commonLikedItems = async (userOneId: string, userTwoId: string) => {};
+export const commonLikedItems = async (
+  userOneId: string,
+  userTwoId: string
+) => {};
