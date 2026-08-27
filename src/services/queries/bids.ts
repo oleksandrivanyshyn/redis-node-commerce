3de97637b7afd932e909037f90eed9d129159911
Oleksandr Ivanyshyn <o.ivanyshyn.dev@gmail.com>
2026-08-27 17:14:41 +0300

    feat: add distributed lock with retry mechanism to ensure safe bid creation

--- src/services/queries/bids.ts
+++ src/services/queries/bids.ts
@@ -1,13 +1,11 @@
 import type { CreateBidAttrs, Bid } from '$services/types';
 import { bidHistoryKey, itemsKey, itemsByPriceKey } from '$services/keys';
-import { client } from '$services/redis';
+import { client, withLock } from '$services/redis';
 import { DateTime } from 'luxon';
 import { getItem } from './items';
 
 export const createBid = async (attrs: CreateBidAttrs) => {
-  return client.executeIsolated(async (isolatedClient) => {
-    await isolatedClient.watch(itemsKey(attrs.itemId));
-
+  return withLock(attrs.itemId, async () => {
     const item = await getItem(attrs.itemId);
 
     if (!item) {
@@ -25,20 +23,51 @@ export const createBid = async (attrs: CreateBidAttrs) => {
       attrs.createdAt.toMillis()
     );
 
-    return isolatedClient
-      .multi()
-      .rPush(bidHistoryKey(attrs.itemId), serialized)
-      .hSet(itemsKey(item.id), {
+    return Promise.all([
+      client.rPush(bidHistoryKey(attrs.itemId), serialized),
+      client.hSet(itemsKey(item.id), {
         bids: item.bids + 1,
         price: attrs.amount,
         highestBidUserId: attrs.userId,
-      })
-      .zAdd(itemsByPriceKey(), {
+      }),
+      client.zAdd(itemsByPriceKey(), {
         value: item.id,
         score: attrs.amount,
-      })
-      .exec();
+      }),
+    ]);
   });
+
+  // return client.executeIsolated(async (isolatedClient) => {
+  // 	await isolatedClient.watch(itemsKey(attrs.itemId));
+
+  // 	const item = await getItem(attrs.itemId);
+
+  // 	if (!item) {
+  // 		throw new Error('Item does not exist');
+  // 	}
+  // 	if (item.price >= attrs.amount) {
+  // 		throw new Error('Bid too low');
+  // 	}
+  // 	if (item.endingAt.diff(DateTime.now()).toMillis() < 0) {
+  // 		throw new Error('Item closed to bidding');
+  // 	}
+
+  // 	const serialized = serializeHistory(attrs.amount, attrs.createdAt.toMillis());
+
+  // 	return isolatedClient
+  // 		.multi()
+  // 		.rPush(bidHistoryKey(attrs.itemId), serialized)
+  // 		.hSet(itemsKey(item.id), {
+  // 			bids: item.bids + 1,
+  // 			price: attrs.amount,
+  // 			highestBidUserId: attrs.userId
+  // 		})
+  // 		.zAdd(itemsByPriceKey(), {
+  // 			value: item.id,
+  // 			score: attrs.amount
+  // 		})
+  // 		.exec();
+  // });
 };
 
 export const getBidHistory = async (
